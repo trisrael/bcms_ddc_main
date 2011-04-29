eec9f3d5a8f9bdf5665af6d42d77e49aa63a81b0
Tristan Goffman <tgoffman@gmail.com>
2011-04-28 18:10:14 -0700

    Initial browsercms app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ddc::Application.config.secret_token = '8f77f21f1994ab21cfef7d726ff60d3f3498764daf64944d5733c9fb6ca8ca802f49b25311b0bbd938255d431645861a6bbeb497448dc43be82cc38508f6ef30'
